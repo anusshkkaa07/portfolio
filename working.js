365b0a34981f5cd6c93442af15e7b1d1fbfedad5
Anushka <92505818+anusshkkaa07@users.noreply.github.com>
2023-09-24 14:19:07 +0530

    Add files via upload

---
+++ working.js
@@ -0,0 +1,15 @@
+function open_techstack(){
+    document.getElementById("techstack").style.width="100%";
+}
+
+function closetechstack(){
+    document.getElementById("techstack").style.width="0%"
+}
+
+function open_projects(){
+    document.getElementById("project").style.width="100%";
+}
+
+function closeproject(){
+    document.getElementById("project").style.width="0%"
+}
